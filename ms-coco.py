03a9b3c11b92bb74ff5d04e858c11fbff64955b1
Tarun Gupta Akirala <tarugupta.92@gmail.com>
2016-03-05 13:08:01 -0500

    Create ms-coco.py

---
+++ ms-coco.py
@@ -0,0 +1,17 @@
+# Skelton of python code
+'''
+
+class ms_coco():
+  #init
+  #validate
+  #helper functions
+  #get roidb
+  #selective search roidb
+  #more helper functions.
+
+
+if __name__ == '__main__':
+    d = datasets.ms_coco('val', '2015')
+    res = d.roidb
+    from IPython import embed; embed()
+'''
