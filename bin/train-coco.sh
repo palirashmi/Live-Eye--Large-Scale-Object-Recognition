604e2d917a3269b035f4c52c8edf42793b84cd48
Tarun Gupta Akirala <takirala@ufl.edu>
2016-04-12 17:56:59 -0400

    Update train-coco.sh

--- bin/train-coco.sh
+++ bin/train-coco.sh
@@ -1,4 +1,7 @@
-# Skelton of Script  to train the model.
+#Training command
 
-# 1. Train
-# 2. Test
+tools/train_net.py --gpu 0 --solver ./models/solvers/solver.prototxt --weights data/imagenet_models/VGG16.v2.caffemodel --imdb coco_2014_train --iters 500 --cfg ./experiments/cfgs/faster_rcnn_end2end.yml
+
+#Testing command
+
+./tools/test_net.py --gpu 0 --def ./models/coco/solvers/test.prototxt --net ./models/bigdata_coco_final.caffemodel --imdb coco_2015_test --cfg experiments/cfgs/faster_rcnn_end2end.yml
